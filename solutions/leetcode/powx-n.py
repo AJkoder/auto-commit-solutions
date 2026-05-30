2cbf1f3b39bca9d7f36a5338d7be64f98e49aba5
AJkoder <jainanshi0306@gmail.com>
2026-05-30 13:39:38 +0530

    [2026-05-30] Solved: powx-n.py (leetcode)

---
+++ solutions/leetcode/powx-n.py
@@ -0,0 +1,10 @@
+# Problem  : Pow(x, n)
+# Difficulty: Medium
+# Tags     : Math, Recursion
+# URL      : https://leetcode.com/problems/powx-n/
+# Solved on: 2026-05-30 13:39
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
